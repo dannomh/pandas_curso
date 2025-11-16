23efe368f2f847c7a0c10a85e7983970a2978b19
Daniel <dannoldu@hotmail.com>
2025-11-16 01:26:17 -0500

    dataframe edad

---
+++ dataframe.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+data = {
+    "nombre": ["Ana", "Luis", "Carla", "Pedro"],
+    "edad": [23, 34, 29, 40],
+    "ciudad": ["Quito", "Guayaquil", "Cuenca", "Quito"],
+    "puntos": [88, 92, 95, 70]
+}
+
+df = pd.DataFrame(data)
+print(df["edad"])# muestra SOLO la columna edad
